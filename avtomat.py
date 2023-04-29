4366547e42f76d1111fd8f851cfe3ff871bc3d74
NastyaBalykina <126290377+NastyaBalykina@users.noreply.github.com>
2023-04-29 13:56:08 +0300

    Update avtomat.py

--- avtomat.py
+++ avtomat.py
@@ -6,19 +6,22 @@ import math
 def angle(x,y):
     return math.atan(x/y)
 
+def distance(x1, y1, x2, y2):
+    return math.sqrt((x2-x1)**2+(y2-y1)**2)
+
 class Robot:
     def __init__(self):
         self.state = "On_station"
-        self.x = 0.0   #Позиция робота
-        self.y = 0.0
-        self.angle = 0.0
-        self.order_place = 0
-        self.target_angle = 0.0 #Позиция цели
-        self.target_x = 0.0
-        self.target_y = 0.0
-        self.station_x = 0.0    #Позиция станции
-        self.station_y = 0.0
-        self.station_angle = 0.0
+        self.x = None   #Позиция робота
+        self.y = None
+        self.angle = None
+        self.order_place = None
+        self.target_angle = None #Позиция цели
+        self.target_x = None
+        self.target_y = None
+        self.station_x = None    #Позиция станции
+        self.station_y = None
+        self.station_angle = None
         self.pouring_state = None
         self.mqtt_client = None #MQTT
         self.stop_thread = False
@@ -27,20 +30,24 @@ class Robot:
         while not self.stop_thread:
             def on_connect(client, userdata, flags, rc):
                 print("MQTT Connected with result code " + str(rc))
-                client.subscribe("camera")
                 client.subscribe("tgbot")
                 client.subscribe("pivo_station")
+                client.subscribe("robot")
+                client.subscribe("station")
+                client.subscribe("target")
 
             def on_message(client, userdata, msg):
                 topic = msg.topic
-                if topic == "tgbot":
+                if topic == "tgbot":    #получает место заказа
                     self.order_place = int(msg)
-                elif topic == "camera":
-                    self.station_x, self.station_y = map(float, msg.payload.decode().split(","))
+                    self.state = "Get_order"
+                elif topic == "robot":  #получает координаты робота
                     self.x, self.y = map(float, msg.payload.decode().split(","))
-                    if self.state == "Bring_order":
-                        self.target_x, self.target_y = map(float, msg.payload.decode().split(","))
-                elif topic == "pivo_station":
+                elif topic == "station":    #получает координаты станции
+                    self.station_x, self.station_y = map(float, msg.payload.decode().split(","))
+                elif topic == "target": #получает координаты цели
+                    self.target_x, self.target_y = map(float, msg.payload.decode().split(","))
+                elif topic == "pivo_station":   #
                     self.pouring_state = "nalito"
 
             if self.mqtt_client is None:
@@ -75,6 +82,22 @@ class Robot:
 
     def start_work_thread(self):
         threading.Thread(target=self.work_thread, daemon=True).start()
+
+    def move_to_customer(self, order_place):
+        #выбрать цель в зависимости от значения order_place
+        while distance(self.x, self.y, self.target_x, self.target_y) >= 0:
+            if self.angle != self.target_angle:
+                    #повернуться
+            else:
+                    #ехать прямо
+
+    def move_to_station(self):
+        # выбрать цель в зависимости от значения order_place
+        while distance(self.x, self.y, self.station_x, self.station_y) >= 0:
+            if self.angle != self.station_angle:
+                # повернуться
+            else:
+                # ехать прямо
 
     def run(self):
         self.start_mqtt_thread()
