90cf0ff3fe02d5faeffbfc15f8b9885fede6e392
NastyaBalykina <126290377+NastyaBalykina@users.noreply.github.com>
2023-05-06 14:32:00 +0300

    Update avtomat.py

--- avtomat.py
+++ avtomat.py
@@ -2,36 +2,44 @@ import time
 import threading
 import paho.mqtt.client as mqtt
 import math
+
 from paho.mqtt import publish
 
-def angle(x,y):
-    return math.atan(x/y)
 
-def cosinus(x1,x2,y1,y2):
-    cos = abs(x1*x2 + y1*y2) / (math.sqrt(x1**2 + y1**2) * math.sqrt(x2**2 + y2**2))
+def angle(x, y):
+    return math.atan(x / y)
+
+
+def cosinus(x1, x2, y1, y2):
+    cos = abs(x1 * x2 + y1 * y2) / (math.sqrt(x1 ** 2 + y1 ** 2) * math.sqrt(x2 ** 2 + y2 ** 2))
     return cos
 
+
 def angle_between_vectors(self):
-    angle = math.acos(cosinus(self.x1,self.x2,self.y1,self.y2))
+    angle = math.acos(cosinus(self.x1, self.x2, self.y1, self.y2))
     return angle
 
+
 def distance(x1, y1, x2, y2):
-    return math.sqrt((x2-x1)**2+(y2-y1)**2)
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 
 class Robot:
     def __init__(self):
         self.state = "On_station"
-        self.x = None   #Позиция робота
-        self.y = None
+        self.pink_x = None  # Позиция робота
+        self.pink_y = None
+        self.blue_x = None  # Позиция робота
+        self.blue_y = None
         self.angle = None
-        self.target_angle = None #Позиция цели
+        self.target_angle = None  # Позиция цели
         self.target_x = None
         self.target_y = None
-        self.station_x = None    #Позиция станции
+        self.station_x = None  # Позиция станции
         self.station_y = None
         self.station_angle = None
         self.pouring_state = None
-        self.mqtt_client = None #MQTT
+        self.mqtt_client = None  # MQTT
         self.stop_thread = False
 
     def mqtt_thread(self):
@@ -43,18 +51,20 @@ class Robot:
                 client.subscribe("robot")
                 client.subscribe("station")
                 client.subscribe("target")
+                client.subscribe("abot/command/alex")
 
             def on_message(client, userdata, msg):
                 topic = msg.topic
-                if topic == "tgbot":    #получает место заказа
+                if topic == "tgbot":  # получает место заказа
                     self.state = "Get_order"
-                elif topic == "robot":  #получает координаты робота
-                    self.x, self.y = map(float, msg.payload.decode().split(","))
-                elif topic == "station":    #получает координаты станции
+                elif topic == "robot":  # получает координаты робота
+                    coords = msg.payload.decode()
+                    self.pink_x, self.pink_y, self.blue_x, self.blue_y = coords.split()
+                elif topic == "station":  # получает координаты станции
                     self.station_x, self.station_y = map(float, msg.payload.decode().split(","))
-                elif topic == "target": #получает координаты цели
+                elif topic == "target":  # получает координаты цели
                     self.target_x, self.target_y = map(float, msg.payload.decode().split(","))
-                elif topic == "pivo_station":   #налито?
+                elif topic == "pivo_station":  # налито?
                     self.pouring_state = "nalito"
 
             if self.mqtt_client is None:
@@ -73,34 +83,52 @@ class Robot:
                 print("люблю доставлять людям счастье и пиво")
                 time.sleep(1)
                 print("Пам-парам, у меня есть пиво!")
-                #стоять чилить
+                # стоять чилить
             elif self.state == "Get_order":
                 if self.pouring_state == "nalito":
                     self.state = "Bring_order"
                     self.pouring_state = None
             elif self.state == "Bring_order":
-                self.move_to_customer(self)
+                self.move_to_customer()
             elif self.state == "Goto_station":
                 self.move_to_station()
                 self.state = "On_station"
 
+    def start_mqtt_thread(self):
+        threading.Thread(target=self.mqtt_thread, daemon=True).start()
+
+    def start_work_thread(self):
+        threading.Thread(target=self.work_thread, daemon=True).start()
+
     def move_to_customer(self):
+        while distance(self.x, self.y, self.target_x, self.target_y) >= 0: #больше не просто х и у!! поменять!!
+        angle = angle_between_vectors(self.robot_vector(self), self.to_target_vector(self))
+        robot_vector_x, robot_vector_y = self.robot_vector(self.pink_x, self.pink_y, self.blue_x, self.blue_y)
+        target_vector_x, target_vector_y = self.to_target_vector(self.target_x, self.target_y, self.station_x, self.station_y)
+        tangens_angle_robot = math.tan(robot_vector_y / robot_vector_x)
+        tangens_angle_target = math.tan(target_vector_y / target_vector_x)
         while distance(self.x, self.y, self.target_x, self.target_y) >= 0:
-            if self.angle != self.target_angle: #повернуться
-                if self.angle > self.target_angle:  #повернуться по часовой
+            if angle != 0:  # повернуться
+                if self.angle > self.target_angle:  # повернуться по часовой
                     msg = "{\"cmd\":\"right\",\"val\":2.0,\"spd\":0.6}"
                     publish("abot/command/alex", msg)
-                elif self.angle < self.target_angle:    #повернуться против часовой
+                elif self.angle < self.target_angle:  # повернуться против часовой
                     msg = "{\"cmd\":\"left\",\"val\":2.0,\"spd\":0.6}"
                     publish("abot/command/alex", msg)
             else:
                 msg = "{\"cmd\":\"forward\",\"val\":2.0,\"spd\":0.6}"
                 publish("abot/command/alex", msg)
-                #ехать прямо
+                # ехать прямо
 
     def move_to_station(self):
-        while distance(self.x, self.y, self.station_x, self.station_y) >= 0:
-            if self.angle != self.station_angle:  # повернуться
+        while distance(self.x, self.y, self.target_x, self.target_y) >= 0:#больше не просто х и у!! поменять!!
+        angle = angle_between_vectors(self.robot_vector(self), self.to_target_vector(self))
+        robot_vector_x, robot_vector_y = self.robot_vector(self.pink_x, self.pink_y, self.blue_x, self.blue_y)
+        target_vector_x, target_vector_y = self.to_target_vector(self.target_x, self.target_y, self.station_x, self.station_y)
+        tangens_angle_robot = math.tan(robot_vector_y / robot_vector_x)
+        tangens_angle_target = math.tan(target_vector_y / target_vector_x)
+        while distance(self.x, self.y, self.target_x, self.target_y) >= 0:
+            if angle != 0:  # повернуться
                 if self.angle > self.station_angle:  # повернуться по часовой
                     msg = "{\"cmd\":\"right\",\"val\":2.0,\"spd\":0.6}"
                     publish("abot/command/alex", msg)
